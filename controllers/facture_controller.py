959ac0e08101efea3c7ea5572d8990308073cfb9
Moon Brite <josemiguelvalecosta@gmail.com>
2024-12-13 14:41:49 +0100

    [feat]:(facture_controller.py) Ajout d'une methode pour le montant total

--- controllers/facture_controller.py
+++ controllers/facture_controller.py
@@ -16,3 +16,16 @@ class FactureController:
             print(resu)
         print(f"Prix Total de la commande :{prix_total} € \n")
         print("==========================")
+
+    def montant_total(self,commande_id,connexion):
+
+        cursor = connexion.cursor()
+        cursor.execute("SELECT * FROM plat_commande WHERE commande_id = %s;", (commande_id,))
+        results = cursor.fetchall()
+        prix_total = 0
+        for result in results:
+            cursor.execute("SELECT * FROM plat WHERE id = %s;", (result[2],))
+            resu = cursor.fetchone()
+            prix_total += resu[3]
+
+        return prix_total
