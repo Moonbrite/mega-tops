ede0919f2758ffe79843547df68f0fc7952acdbc
Moon Brite <josemiguelvalecosta@gmail.com>
2024-12-13 10:50:46 +0100

    [feat]:(main.py,table_controller.py) Ajout du controller table_controller.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from controllers.table_controller import Table_controller
 from database.db import connect_db
 from database.seeds import creer_tables, inserer_data
 
@@ -10,13 +11,21 @@ def afficher_menu_principal():
     print("4. Quitter")
     return input("Choisissez une option : ")
 
+def switch_action(action,connexion):
+    match action:
+        case "1":
+            table_controller = Table_controller()
+            table_controller.update_table(connexion)
+
+
 def main():
     connexion = connect_db()
 
     if connexion:
-        creer_tables(connexion)
-        inserer_data(connexion)
-        afficher_menu_principal()
+        #creer_tables(connexion)
+        #inserer_data(connexion)
+
+        switch_action(afficher_menu_principal(),connexion)
         connexion.close()
 
 
