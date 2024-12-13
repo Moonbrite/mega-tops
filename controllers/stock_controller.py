ceac12714c7015da1b02c905c5d4c6e42f1affca
RED <qati.rejda@gmail.com>
2024-12-13 11:57:34 +0100

    command controller début

---
+++ controllers/stock_controller.py
@@ -0,0 +1,7 @@
+class StockController:
+    def __init__(self):
+        pass
+
+    def reduire_stock_par_plat(self, plat_id, quantite):
+        print(f"Réduction du stock pour le plat {plat_id} avec quantité {quantite}.")
+
